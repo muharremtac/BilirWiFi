f3600253a4862799506dd2921634a7f891db217e
Muharrem Taç <mtac@perfektive.com>
2022-05-16 04:23:22 +0300

    Kablosuz ağ ayarlarında Türkçe desteği

--- libs/reset_device/reset.py
+++ libs/reset_device/reset.py
@@ -33,18 +33,18 @@ if reboot_required == True:
 
 # This is the main logic loop waiting for a button to be pressed on GPIO 18 for 10 seconds.
 # If that happens the device will reset to its AP Host mode allowing for reconfiguration on a new network.
-while True:
-    while GPIO.input(18) == 1:
-        time.sleep(1)
-        counter = counter + 1
-
-        print(counter)
-
-        if counter == 9:
-            reset_lib.reset_to_host_mode()
-
-        if GPIO.input(18) == 0:
-            counter = 0
-            break
-
-    time.sleep(1)
+# while True:
+#     while GPIO.input(18) == 1:
+#         time.sleep(1)
+#         counter = counter + 1
+#
+#         print(counter)
+#
+#         if counter == 9:
+#             reset_lib.reset_to_host_mode()
+#
+#         if GPIO.input(18) == 0:
+#             counter = 0
+#             break
+#
+#     time.sleep(1)
--- libs/reset_device/reset_lib.py
+++ libs/reset_device/reset_lib.py
@@ -78,9 +78,7 @@ def is_wifi_active():
 
 def reset_to_host_mode():
 	if not os.path.isfile('/etc/raspiwifi/host_mode'):
-		os.system('aplay /usr/lib/raspiwifi/reset_device/button_chime.wav')
 		os.system('rm -f /etc/wpa_supplicant/wpa_supplicant.conf')
-		os.system('rm -f /home/pi/Projects/RaspiWifi/tmp/*')
 		os.system('rm /etc/cron.raspiwifi/apclient_bootstrapper')
 		os.system('cp /usr/lib/raspiwifi/reset_device/static_files/aphost_bootstrapper /etc/cron.raspiwifi/')
 		os.system('chmod +x /etc/cron.raspiwifi/aphost_bootstrapper')
